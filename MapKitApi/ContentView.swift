7d7ea3750aad3e1882d54d34dad7567e7cc57665
zi9987 <peterluoluo25@gmail.com>
2024-07-26 15:17:18 +0800

    Initial Commit

---
+++ MapKitApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitApi
+//
+//  Created by 羅子淵 on 2024/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitApi/MapKitApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitApiApp.swift
+//  MapKitApi
+//
+//  Created by 羅子淵 on 2024/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MapKitApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
